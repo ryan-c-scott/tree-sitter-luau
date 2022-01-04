48ef4e216fc7bf1ad4e2839dd4f273a9326e5253
Ryan C. Scott <ryan@5pmcasual.com>
2022-01-03 16:13:43 -0800

    Added proper number literal support

--- grammar.js
+++ grammar.js
@@ -11,9 +11,39 @@ module.exports = grammar({
 
     // NOTE: Unspecified in spec
     NAME: $ => /[a-zA-Z_][a-zA-Z0-9_]*/,
-    NUMBER: $ => /\d+/,
     STRING: $ => choice(/"(\\.|[^\"])*"/, /'(\\.|[^\'])*'/),
     comment: ($) => seq("--", /.*\r?\n/),
+    NUMBER: $ => {
+      const decimal_digits = /[0-9]+/
+      const signed_integer = seq(optional(choice('-', '+')), decimal_digits)
+      const decimal_exponent_part = seq(choice('e', 'E'), signed_integer)
+
+      const decimal_integer_literal = choice(
+        '0',
+        seq(optional('0'), /[1-9]/, optional(decimal_digits))
+      )
+
+      const hex_digits = /[a-fA-F0-9]+/
+      const hex_exponent_part = seq(choice('p', 'P'), signed_integer)
+
+      const decimal_literal = choice(
+        seq(decimal_integer_literal, '.', optional(decimal_digits), optional(decimal_exponent_part)),
+        seq('.', decimal_digits, optional(decimal_exponent_part)),
+        seq(decimal_integer_literal, optional(decimal_exponent_part))
+      )
+
+      const hex_literal = seq(
+        choice('0x', '0X'),
+        hex_digits,
+        optional(seq('.', hex_digits)),
+        optional(hex_exponent_part)
+      )
+
+      return token(choice(
+        decimal_literal,
+        hex_literal
+      ))
+    },
 
     //
     laststat: $ => prec.right(choice(seq('return', optional($.explist)), 'break', 'continue')),
