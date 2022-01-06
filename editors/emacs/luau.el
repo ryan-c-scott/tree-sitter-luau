fe740ab05084286a1a1792d6006afd53d6376a25
Ryan C. Scott <ryan@5pmcasual.com>
2022-01-05 16:45:16 -0800

    Added cleaner structure for emacs integration code

--- editors/emacs/luau.el
+++ editors/emacs/luau.el
@@ -1,19 +1,16 @@
 ;; zig build-lib -I src/ src/parser.c src/scanner.cc -dynamic --library c++ --name luau
 
-(progn
-  (add-to-list 'tree-sitter-major-mode-language-alist '(lua-mode . luau))
-  (tree-sitter-require 'luau)
-
-  (defun luau-tree-sitter-setup ()
-    (interactive)
-    (setq tree-sitter-hl-default-patterns "
+(defun luau-tree-sitter-setup ()
+  (interactive)
+  (setq tree-sitter-hl-default-patterns "
 (STRING) @string
 (NUMBER) @number
 (comment) @comment
 
 (binding (NAME) @variable (Type) @type)
 (ReturnType) @type
-(functioncall (NAME) @function.call)
+(functioncall (NAME) @function.call !method)
+(functioncall method: (NAME) @method.call)
 (functioncall (prefixexp (var) @function.call))
 (funcname) @function
 (stat function_local: (NAME) @function)
@@ -25,9 +22,11 @@
 (ERROR) @comment
 
 (laststat [\"return\" \"break\" \"continue\"] @keyword)
+(stat export: \"export\" @constant \"type\" @keyword (NAME) @type)
 
 [\"end\" \"or\" \"for\" \"in\" \"and\" \"else\" \"elseif\" \"do\" \"then\" \"function\" \"if\" \"return\" \"local\"] @keyword
 ")
-    (tree-sitter-hl-mode 1))
+  (add-to-list 'tree-sitter-major-mode-language-alist '(lua-mode . luau))
+  (tree-sitter-require 'luau))
 
-  (add-hook 'lua-mode-hook 'luau-tree-sitter-setup))
+(add-hook 'lua-mode-hook 'luau-tree-sitter-setup)
